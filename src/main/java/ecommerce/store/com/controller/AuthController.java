086ce91824906b92877fa234ae434bd10bb02410
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-10-15 17:21:12 +0100

    "Add AuthController with base structure"

---
+++ src/main/java/ecommerce/store/com/controller/AuthController.java
@@ -0,0 +1,17 @@
+package ecommerce.store.com.controller;
+
+
+import ecommerce.store.com.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    @Autowired
+   private UserServiceImpl userService;
+
+
+}
