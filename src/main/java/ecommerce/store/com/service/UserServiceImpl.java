d590aa7a6263673b715aa88437ad2b76f2bd28d2
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-10-23 01:41:56 +0100

    "Refactor UserServiceImpl to use UserMapper, add password encoding, and improve registration and login logic"

--- src/main/java/ecommerce/store/com/service/UserServiceImpl.java
+++ src/main/java/ecommerce/store/com/service/UserServiceImpl.java
@@ -1,18 +1,23 @@
 package ecommerce.store.com.service;
 
+import ecommerce.store.com.Mapper.UserMapper;
+import ecommerce.store.com.data.model.User;
 import ecommerce.store.com.data.repository.UserRepository;
+import ecommerce.store.com.dtos.requestDto.UserLoginRequestDto;
+import ecommerce.store.com.dtos.requestDto.UserRequest;
 import ecommerce.store.com.dtos.responseDto.UserResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
 
-    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-
+    private final PasswordEncoder passwordEncoder;
 
 
     @Autowired
@@ -22,14 +27,27 @@ public class UserServiceImpl implements UserService{
 
 
     @Override
-    public UserResponse registerUser(UserResponse userRequest) {
+    public UserResponse registerUser(UserRequest userRequest) {
+        if(!userRepository.existsByEmail(userRequest.getEmail())){
+            throw new RuntimeException("Email already exists");
+        }
 
-       return null;
+        User user = UserMapper.toEntity(dto, passwordEncoder.encode(dto.getPassword()));
+        User savedUser = userRepository.save(user);
+
+        return UserMapper.toResponse(savedUser);
     }
 
     @Override
-    public UserResponse loginUser(UserResponse userRequest) {
-        return null;
+    public UserResponse loginUser(UserLoginRequestDto userRequest) {
+        User user = userRepository.findByEmail(userRequest.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if(!passwordEncoder.matches(userRequest.getPassword(), user.getPassword())){
+            throw new RuntimeException("Invalid password");
+        }
+
+        return UserMapper.toResponse(user);
     }
 
 
