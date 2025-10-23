1721b8f3ecca5ad64bb78c0c81692e1974403c7c
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-10-23 01:41:32 +0100

    "Add ResourceNotFoundException for handling missing resources"

---
+++ src/main/java/ecommerce/store/com/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package ecommerce.store.com.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
