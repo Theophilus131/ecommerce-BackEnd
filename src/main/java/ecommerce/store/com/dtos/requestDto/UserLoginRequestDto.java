04f43dc689d5fd647a32c8c95198fe9f731e0a10
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-10-23 01:41:39 +0100

    "Add UserLoginRequestDto for handling login request data"

---
+++ src/main/java/ecommerce/store/com/dtos/requestDto/UserLoginRequestDto.java
@@ -0,0 +1,21 @@
+package ecommerce.store.com.dtos.requestDto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserLoginRequestDto {
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+}
